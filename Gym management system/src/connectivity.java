eb1f541424fcc8aaa70961e0375b343caac75aba
unknown <aakashnanaware2000@gmail.com>
2024-03-10 10:57:59 +0530

    commit changes

---
+++ Gym management system/src/connectivity.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class connectivity {
+    final static String JDBC_DRIVER="com.mysql.jdbc.Driver";
+    final static String DB_URL="jdbc:mysql://localhost:3306/gym";
+    final static String USER="root";
+    final static String PASS="";
+    
+    public static Connection openConnection(){
+        try
+        {
+        Class.forName(JDBC_DRIVER);
+        Connection conn=(Connection)DriverManager.getConnection(DB_URL,USER,PASS);
+        return conn;
+    }catch(Exception e){
+        JOptionPane.showMessageDialog(null, e);
+        return null;   
+    }
+    }
+    
+}
